046da9841ec499fd45527c731eea5b31d49e8660
yestevezp <jeffersson1987@gmail.com>
2015-03-02 09:35:20 -0500

    Archivos Base - Origen de los Tiempos

---
+++ index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="es"> <![endif]-->
+<!--[if IE 7]> <html class="no-js lt-ie9 lt-ie8" lang="es"> <![endif]-->
+<!--[if IE 8]> <html class="no-js lt-ie9" lang="es"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="es"> <!--<![endif]-->
+<head>
+<meta charset="utf-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<title>Index</title>
+<meta name="description" content="Descripción del portal" />
+<meta name="viewport" content="width=device-width,initial-scale=1, maximum-scale=1">
+
+
+
+
+<!--[if lt IE 9]>
+<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+</head>
+
+<body>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="javascripts/jquery-2.1.3.min.js"><\/script>')</script>
+</body>
+</html>
